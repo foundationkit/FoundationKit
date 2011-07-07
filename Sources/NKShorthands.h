e1ec16ef185995030a33fcd047f5dd657c1d7ad7
Erik Aigner <erik@chocomoko.com>
2011-07-07 17:05:26 +0200

    equivalent of foundation functions for CFRange (location and max)

--- Sources/NKShorthands.h
+++ Sources/NKShorthands.h
@@ -5,3 +5,7 @@
 
 // Four char codes
 #define NKFcc(code) (char[5]){(code >> 24) & 0xFF, (code >> 16) & 0xFF, (code >> 8) & 0xFF, code & 0xFF, 0}
+
+// Foundation functions for CF
+#define CFMaxRange(r) (r.location + r.length)
+#define CFLocationInRange(l, r) (l >= r.location ? (l <= CFMaxRange(r) ? YES : NO) : NO)
