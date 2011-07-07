4d82e11d1b298ecdf969271f2d1da32b658f68fb
Erik Aigner <erik@chocomoko.com>
2011-07-07 20:15:07 +0200

    this should of course read NK_INLINE

--- Sources/NKShorthands.h
+++ Sources/NKShorthands.h
@@ -1,15 +1,15 @@
 // Part of FoundationKit http://foundationk.it
 
-#ifndef RK_INLINE
-#define RK_INLINE static inline
+#ifndef NK_INLINE
+#define NK_INLINE static inline
 #endif
 
 // Localization
 #define _(x) NSLocalizedString(x, nil)
 
 // Four char codes
-RK_INLINE char * NKFcc(code) { return (char[5]){(code >> 24) & 0xFF, (code >> 16) & 0xFF, (code >> 8) & 0xFF, code & 0xFF, 0}; }
+NK_INLINE char * NKFcc(code) { return (char[5]){(code >> 24) & 0xFF, (code >> 16) & 0xFF, (code >> 8) & 0xFF, code & 0xFF, 0}; }
 
 // Foundation functions for CF
-RK_INLINE CFIndex CFMaxRange(CFRange range) { return (range.location + range.length); }
-RK_INLINE Boolean CFLocationInRange(CFIndex location, CFRange range) { return (location >= range.location ? (location <= CFMaxRange(range) ? YES : NO) : NO); }
+NK_INLINE CFIndex CFMaxRange(CFRange range) { return (range.location + range.length); }
+NK_INLINE Boolean CFLocationInRange(CFIndex location, CFRange range) { return (location >= range.location ? (location <= CFMaxRange(range) ? YES : NO) : NO); }
