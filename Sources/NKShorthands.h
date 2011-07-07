fcca508515ad59fe29d2af4434cfdf13e32f5cb7
Erik Aigner <erik@chocomoko.com>
2011-07-07 20:13:55 +0200

    RK_INLINE instead of macros

--- Sources/NKShorthands.h
+++ Sources/NKShorthands.h
@@ -1,11 +1,15 @@
 // Part of FoundationKit http://foundationk.it
 
+#ifndef RK_INLINE
+#define RK_INLINE static inline
+#endif
+
 // Localization
 #define _(x) NSLocalizedString(x, nil)
 
 // Four char codes
-#define NKFcc(code) (char[5]){(code >> 24) & 0xFF, (code >> 16) & 0xFF, (code >> 8) & 0xFF, code & 0xFF, 0}
+RK_INLINE char * NKFcc(code) { return (char[5]){(code >> 24) & 0xFF, (code >> 16) & 0xFF, (code >> 8) & 0xFF, code & 0xFF, 0}; }
 
 // Foundation functions for CF
-#define CFMaxRange(r) (r.location + r.length)
-#define CFLocationInRange(l, r) (l >= r.location ? (l <= CFMaxRange(r) ? YES : NO) : NO)
+RK_INLINE CFIndex CFMaxRange(CFRange range) { return (range.location + range.length); }
+RK_INLINE Boolean CFLocationInRange(CFIndex location, CFRange range) { return (location >= range.location ? (location <= CFMaxRange(range) ? YES : NO) : NO); }
