8e6d7c3bd7889b1855a80f8ad8a009c635b54f86
Matthias Tretter <matthias.tretter@gmail.com>
2011-07-09 21:26:39 +0200

    Renamed NKConcise-Categories from XY-NKConcise.h to XY+NKConcise.h for consistency

--- Sources/FoundationKit.h
+++ Sources/FoundationKit.h
@@ -12,12 +12,12 @@
 #import "NSMutableDictionary+NKBlocks.h"
 
 // ConciseKit adoptions
-#import "NSArray-NKConcise.h"
-#import "NSDictionary-NKConcise.h"
-#import "NSNull-NKConcise.h"
-#import "NSNumber-NKConcise.h"
-#import "NSString-NKConcise.h"
-#import "NSValue-NKConcise.h"
+#import "NSArray+NKConcise.h"
+#import "NSDictionary+NKConcise.h"
+#import "NSNull+NKConcise.h"
+#import "NSNumber+NKConcise.h"
+#import "NSString+NKConcise.h"
+#import "NSValue+NKConcise.h"
 
 #import "NSData+NKCrypto.h"
 #import "NSError+NKAdditions.h"
