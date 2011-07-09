f66556c2deadcdfa2d7bba359506ee4053f668ac
Matthias Tretter <matthias.tretter@gmail.com>
2011-07-10 00:00:43 +0200

    Added category on NSObject for concise associated objects (originally from Andy Matuschek)

---
+++ Sources/NSObject+NKAssociatedObjects.h
@@ -0,0 +1,14 @@
+// Part of FoundationKit http://foundationk.it
+//
+// Originally created by Andy Matuschek in public domain, because he loves us:
+// https://github.com/andymatuschak/NSObject-AssociatedObjects
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (NKAssociatedObjects)
+
+- (void)associateValue:(id)value withKey:(void *)key;
+- (void)weaklyAssociateValue:(id)value withKey:(void *)key;
+- (id)associatedValueForKey:(void *)key;
+
+@end
---
+++ Sources/NSObject+NKAssociatedObjects.m
@@ -0,0 +1,23 @@
+// Part of FoundationKit http://foundationk.it
+//
+// Originally created by Andy Matuschek in public domain, because he loves us:
+// https://github.com/andymatuschak/NSObject-AssociatedObjects
+
+#import "NSObject+NKAssociatedObjects.h"
+#import <objc/runtime.h>
+
+@implementation NSObject (NKAssociatedObjects)
+
+- (void)associateValue:(id)value withKey:(void *)key {
+	objc_setAssociatedObject(self, key, value, OBJC_ASSOCIATION_RETAIN);
+}
+
+- (void)weaklyAssociateValue:(id)value withKey:(void *)key {
+	objc_setAssociatedObject(self, key, value, OBJC_ASSOCIATION_ASSIGN);
+}
+
+- (id)associatedValueForKey:(void *)key {
+	return objc_getAssociatedObject(self, key);
+}
+
+@end
