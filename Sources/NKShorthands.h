2cee74f6fd3a6a4d568d16b57f737148483b2fd6
Matthias Tretter <matthias.tretter@gmail.com>
2011-07-09 23:52:42 +0200

    Added function to check for empty object

--- Sources/NKShorthands.h
+++ Sources/NKShorthands.h
@@ -1,4 +1,7 @@
 // Part of FoundationKit http://foundationk.it
+//
+// IsEmpty taken from http://www.wilshipley.com/blog/2005/10/pimp-my-code-interlude-free-code.html
+
 
 #import <Foundation/Foundation.h>
 
@@ -12,3 +15,10 @@ NS_INLINE char * NKFcc(code) { return (char[5]){(code >> 24) & 0xFF, (code >> 16
 // Foundation functions for CF
 NS_INLINE CFIndex CFMaxRange(CFRange range) { return (range.location + range.length); }
 NS_INLINE Boolean CFLocationInRange(CFIndex location, CFRange range) { return (location >= range.location ? (location <= CFMaxRange(range) ? YES : NO) : NO); }
+
+
+NS_INLINE BOOL NKIsEmpty(id object) {
+	return object == nil || ([object isEqual:[NSNull null]]) ||
+  ([object respondsToSelector:@selector(length)] && [(NSData *)object length] == 0) ||
+  ([object respondsToSelector:@selector(count)]  && [(NSArray *)object count] == 0);
+}
