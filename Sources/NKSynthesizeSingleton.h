d2c853a07d6622f5981a5b4f039cbeb34cb21148
Martin Schürrer <martin@schuerrer.org>
2011-07-10 09:38:46 +0200

    Revert "Remove NKSynthesizeSingleton" as per @steipete
    This reverts commit af03b1504111edff3c688e85b5e1f4db1f2432d7.

---
+++ Sources/NKSynthesizeSingleton.h
@@ -0,0 +1,47 @@
+// Part of FoundationKit http://foundationk.it
+// 
+//  Based on original work by Matt Gallagher on 20/10/08.
+//  Copyright 2009 Matt Gallagher. All rights reserved.
+//
+//  Permission is given to use this source code file without charge in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
+
+#define NKSynthesizeSingletonForClass(classname) \
+\
+static classname *shared##classname = nil; \
+\
++ (classname *)shared##classname \
+{ \
+static dispatch_once_t pred; \
+dispatch_once(&pred, ^{ shared##classname = [[self alloc] init]; }); \
+return shared##classname; \
+} \
+\
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return self; \
+} \
+\
+- (id)retain \
+{ \
+return self; \
+} \
+\
+- (NSUInteger)retainCount \
+{ \
+return NSUIntegerMax; \
+} \
+\
+- (oneway void)release \
+{ \
+} \
+\
+- (id)autorelease \
+{ \
+return self; \
+}
