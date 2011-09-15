e542d7e7e0d0e6bc485a7258bff262618c5e9d12
Matthias Tretter <matthias.tretter@gmail.com>
2011-09-15 20:17:05 +0200

    Moved macros for points/rects and sizes to MacKit

--- Sources/NSValue+FKConcise.h
+++ Sources/NSValue+FKConcise.h
@@ -4,7 +4,4 @@
 
 #define $nonretained(val) [NSValue valueWithNonretainedObject:(val)]
 #define $pointer(val)     [NSValue valueWithPointer:(val)]
-#define $point(val)       [NSValue valueWithPoint:(val)]
 #define $range(val)       [NSValue valueWithRange:(val)]
-#define $rect(val)        [NSValue valueWithRect:(val)]
-#define $size(val)        [NSValue valueWithSize:(val)]
