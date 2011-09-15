2ac00b5b0852c30495899460f1a139cf9d29d178
Matthias Tretter <matthias.tretter@gmail.com>
2011-09-15 17:28:34 +0200

    Added category on NSUserDefaults to register defaults from Settings-Bundle

---
+++ Sources/NSUserDefaults+FKAdditions.m
@@ -0,0 +1,35 @@
+// Part of FoundationKit http://foundationk.it
+
+#import "NSUserDefaults+FKAdditions.h"
+
+@implementation NSUserDefaults (FKAdditions)
+
+- (void)registerDefaultsFromBundle:(NSString *)bundle file:(NSString *)file {
+  NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:[bundle stringByAppendingPathComponent:file]];
+  NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
+  NSMutableDictionary *defaultsToRegister = [[NSMutableDictionary alloc] initWithCapacity:[preferences count]];
+  
+  for(NSDictionary *prefSpecification in preferences) {
+    NSString *key = [prefSpecification objectForKey:@"Key"];
+    id defaultValue = [prefSpecification objectForKey:@"DefaultValue"];
+    
+    if(key && defaultValue) {
+      [defaultsToRegister setObject:defaultValue forKey:key];
+    }
+  }
+  
+  [[NSUserDefaults standardUserDefaults] registerDefaults:defaultsToRegister];
+}
+
+- (void)registerDefaultsFromSettingsBundle {
+  NSString *settingsBundle = [[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"];
+  
+  if(!settingsBundle) {
+    // DDLogWarn(@"Could not find Settings.bundle");
+    return;
+  }
+  
+  [self registerDefaultsFromBundle:settingsBundle file:@"Root.plist"];
+}
+
+@end
