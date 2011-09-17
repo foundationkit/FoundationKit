5a806880586d7034a4b50656a467a4781e855adc
Matthias Tretter <matthias.tretter@gmail.com>
2011-09-17 13:39:43 +0200

    Do not make dispatch-functions inline

--- Sources/FKDispatch.h
+++ Sources/FKDispatch.h
@@ -1,9 +1,9 @@
-// Part of FOundationKit http://foundationk.it
+// Part of FoundationKit http://foundationk.it
 
 #import <Foundation/Foundation.h>
 
 // do not block queue when called on same queue we want to dispatch to
-NS_INLINE void dispatch_sync_reentrant(dispatch_queue_t queue, dispatch_block_t block);
+void dispatch_sync_reentrant(dispatch_queue_t queue, dispatch_block_t block);
 
 // same as above, shortcut for main_queue
-NS_INLINE void dispatch_sync_on_main_queue(dispatch_block_t block);
+void dispatch_sync_on_main_queue(dispatch_block_t block);
