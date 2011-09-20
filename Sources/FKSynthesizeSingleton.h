e1328fe851300dd89d28cca41fd53b972487cce9
Matthias Tretter <matthias.tretter@gmail.com>
2011-09-20 22:43:28 +0200

    New block-based method for defining a singleton

--- Sources/FKSynthesizeSingleton.h
+++ Sources/FKSynthesizeSingleton.h
@@ -1,4 +1,6 @@
 // Part of FoundationKit http://foundationk.it
+//
+// FKDefineSingletonUsingBlock derived from lukeredpath's gist: https://gist.github.com/1057420
 
 #define FKSynthesizeSingletonForClass(classname) \
 \
@@ -10,3 +12,17 @@ static dispatch_once_t pred; \
 dispatch_once(&pred, ^{ shared##classname = [[self alloc] init]; }); \
 return shared##classname; \
 }
+
+
+// Use like this:
+// + (id)sharedInstance {
+//  FKDefineSingletonUsingBlock(^{
+//    return [[self alloc] init];
+//  });
+// }
+
+#define FKDefineSingletonUsingBlock(block) \
+static dispatch_once_t pred; \
+__strong static id sharedObject_ = nil; \
+dispatch_once(&pred, ^{ sharedObject_ = block(); }); \
+return _sharedObject; \
