4e76ca29e6f8d950c1927c912f2d4ed9bd26e4c5
Matthias Tretter <matthias.tretter@gmail.com>
2011-09-22 21:43:57 +0200

    Make dispatch functions inline

--- Sources/FKDispatch.h
+++ Sources/FKDispatch.h
@@ -4,4 +4,20 @@
 
 // do not block queue when called on same queue we want to dispatch to
 // WARNING: do not use on main_queue
-void dispatch_sync_reentrant(dispatch_queue_t queue, dispatch_block_t block);
+NS_INLINE void dispatch_sync_reentrant(dispatch_queue_t queue, dispatch_block_t block) {
+	if (dispatch_get_current_queue() == queue) {
+		block();
+	} else {
+		dispatch_sync(queue, block);
+	}
+}
+
+// NSThread isMainThread might be true on other queues than the main queue too
+// but it works for the general case of "UI Update on main thread"
+NS_INLINE void dispatch_sync_on_main_queue(dispatch_block_t block) {
+  if ([NSThread isMainThread]) {
+    block();
+  } else {
+    dispatch_sync(dispatch_get_main_queue(), block);
+  }
+}
