73553e60a85eb200162ad784d901d71f856479e9
Matthias Tretter <matthias.tretter@gmail.com>
2011-10-08 00:08:54 +0200

    Concise possibility to get number of rows in section from NSFetchedResultsController

--- Sources/Optional/Core Data/FoundationKit+CoreData.h
+++ Sources/Optional/Core Data/FoundationKit+CoreData.h
@@ -1,4 +1,8 @@
 // Part of FoundationKit http://foundationk.it
 
 #import "NSManagedObject+FKAdditions.h"
+
+#if TARGET_OS_IPHONE
+#import "NSFetchedResultsController+FKAdditions.h"
 #import "UIViewController+FKAnimatedFetchedResultsController.h"
+#endif
