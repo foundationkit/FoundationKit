e14eb2a457c8ee6e4b52054a898c02ebe2d2c1fc
Matthias Tretter <matthias.tretter@gmail.com>
2013-03-22 15:09:42 +0100

    Remove Core Data stuff

--- Sources/Optional/Core Data/FoundationKit+CoreData.h
+++
@@ -1,8 +0,0 @@
-// Part of FoundationKit http://foundationk.it
-
-#import "NSManagedObject+FKAdditions.h"
-
-#if TARGET_OS_IPHONE
-#import "NSFetchedResultsController+FKAdditions.h"
-#import "FKFetchedResultsControllerUpdater.h"
-#endif
