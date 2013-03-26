87c48f34118f43bce7db438570778530523db2d2
Matthias Tretter <matthias.tretter@gmail.com>
2013-03-26 17:08:22 +0100

    Support for decoding ampersand

--- Sources/NSString+FKXML.m
+++ Sources/NSString+FKXML.m
@@ -6,32 +6,34 @@
 - (NSString *)stringByDecodingXMLEntities {
   NSUInteger myLength = [self length];
   NSUInteger ampIndex = [self rangeOfString:@"&" options:NSLiteralSearch].location;
-  
+
   // Short-circuit if there are no ampersands.
   if (ampIndex == NSNotFound) {
     return self;
   }
+  
   // Make result string with some extra capacity.
   NSMutableString *result = [NSMutableString stringWithCapacity:(myLength * 1.25)];
-  
-  // First iteration doesn't need to scan to & since we did that already, 
+
+  // First iteration doesn't need to scan to & since we did that already,
   // but for code simplicity's sake we'll do it again with the scanner.
   NSScanner *scanner = [NSScanner scannerWithString:self];
+  NSCharacterSet *boundaryCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@" \t\n\r;"];
+
   scanner.caseSensitive = YES;
   [scanner setCharactersToBeSkipped:nil];
-  NSCharacterSet *boundaryCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@" \t\n\r;"];
-  
+
   do {
     // Scan up to the next entity or the end of the string.
     NSString *nonEntityString;
-    
+
     if ([scanner scanUpToString:@"&" intoString:&nonEntityString]) {
       [result appendString:nonEntityString];
     }
     if ([scanner isAtEnd]) {
       break;
     }
-    
+
     // Scan either a HTML or numeric character entity reference.
     if ([scanner scanString:@"&amp;" intoString:NULL]) {
       [result appendString:@"&"];
@@ -55,45 +57,47 @@
       [result appendString:@"ü"];
     } else if ([scanner scanString:@"&auml;" intoString:NULL]) {
       [result appendString:@"ä"];
-    } else if ([scanner scanString:@"&szlig;" intoString:NULL]) { 
+    } else if ([scanner scanString:@"&szlig;" intoString:NULL]) {
       [result appendString:@"ß"];
-    } else if ([scanner scanString:@"&nbsp;" intoString:NULL]) {  
+    } else if ([scanner scanString:@"&nbsp;" intoString:NULL]) {
       [result appendString:@" "];
     } else if ([scanner scanString:@"&ndash;" intoString:NULL]) {
       [result appendString:@"–"];
     } else if ([scanner scanString:@"&mdash;" intoString:NULL]) {
       [result appendString:@"—"];
+    } else if ([scanner scanString:@"&#39;" intoString:NULL]) {
+      [result appendString:@"'"];
     } else if ([scanner scanString:@"&#" intoString:NULL]) {
       BOOL gotNumber;
       unsigned charCode;
       NSString *xForHex = @"";
-      
+
       // Is it hex or decimal?
       if ([scanner scanString:@"x" intoString:&xForHex]) {
         gotNumber = [scanner scanHexInt:&charCode];
       } else {
         gotNumber = [scanner scanInt:(int*)&charCode];
       }
-      
+
       if (gotNumber) {
         [result appendFormat:@"%u", charCode];
         [scanner scanString:@";" intoString:NULL];
       } else {
         NSString *unknownEntity = @"";
-        
+
         [scanner scanUpToCharactersFromSet:boundaryCharacterSet intoString:&unknownEntity];
         [result appendFormat:@"&#%@%@", xForHex, unknownEntity];
-        
+
         FKLogDebug(@"Expected numeric character entity but got &#%@%@;", xForHex, unknownEntity);
       }
     } else {
       NSString *amp;
-      
+
       [scanner scanString:@"&" intoString:&amp];      //an isolated & symbol
       [result appendString:amp];
     }
   } while (![scanner isAtEnd]);
-  
+
   return result;
 }
 
