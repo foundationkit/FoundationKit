d48547127748f14d86145bc39e6fe9f43156e8b8
Matthias Tretter <matthias.tretter@gmail.com>
2013-04-06 18:09:46 +0200

    Shortcut for dispatch_after to dispatch on main queue after a given delay in seconds

--- Sources/FKDispatch.h
+++ Sources/FKDispatch.h
@@ -9,7 +9,7 @@
  The identifier of the queue will be "com.AppName.AppVersion.QueueName"
 
  Usage:
- 
+
  FKDefineGCDQueueWithName(foundation_queue);
  // ...
  dispatch_sync(foundation_queue(), ^{ NSLog(@"Logging on a queue."); });
@@ -21,8 +21,16 @@ NS_INLINE dispatch_queue_t QUEUE_NAME(void) {                                   
   static dispatch_once_t onceToken;                                                           \
   dispatch_once(&onceToken, ^{                                                                \
     NSString *queueIdentifier = [NSString stringWithFormat:@"com.%@.%@.%s",                   \
-                                 FKApplicationName(), FKApplicationVersion(), #QUEUE_NAME];   \
+     FKApplicationName(), FKApplicationVersion(), #QUEUE_NAME];   \
     fk_##QUEUE_NAME = dispatch_queue_create([queueIdentifier UTF8String], 0);                 \
   });                                                                                         \
   return fk_##QUEUE_NAME;                                                                     \
+}
+
+
+NS_INLINE void fk_dispatch_after(NSTimeInterval delayInSeconds, dispatch_block_t block) {
+  if (block != nil) {
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), block);
+  }
 }
